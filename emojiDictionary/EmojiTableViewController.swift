eb66bb5129b5939a870db8f1fcd4852f444ee9e6
Rob Pecoraro <rfpecoraro@gmail.com>
2018-09-13 22:19:37 -0400

    1st phase
    - built table view controller. (emojiTableViewController.swift)
    - Added 2nd view controller.

---
+++ emojiDictionary/EmojiTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiTableViewController.swift
+//  emojiDictionary
+//
+//  Created by admin on 9/13/18.
+//  Copyright © 2018 RobPecoraro. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var emoji = ["🔫","😀","🤪","😱","💩","💯","😍","🥑","🌪"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return emoji.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "firstCell", for: indexPath)
+
+       cell.textLabel?.text = emoji[indexPath.row]
+       
+        
+        return cell
+    }
+}
